da071df48a1cf1a2d20af3fdb9ac2fe777a87c4f
AlexanderKarpunin <49994019+AlexanderKarpunin@users.noreply.github.com>
2019-04-26 19:10:54 +0300

    Update install-composer.sh

--- install-composer.sh
+++ install-composer.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
+#EXPECTED_SIGNATURE="$(wget -q -O - https://composer.github.io/installer.sig)"
+#php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+#ACTUAL_SIGNATURE="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+#
+#if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
+#then
+#    >&2 echo 'ERROR: Invalid installer signature'
+#    rm composer-setup.php
+#    exit 1
+#fi
+#
+#php composer-setup.php --quiet
+#RESULT=$?
+#rm composer-setup.php
+#exit $RESULT
+
 EXPECTED_SIGNATURE="$(wget -q -O - https://composer.github.io/installer.sig)"
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 ACTUAL_SIGNATURE="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
@@ -11,8 +27,14 @@ then
     exit 1
 fi
 
-php composer-setup.php --quiet
+#php composer-setup.php --quiet
+php composer-setup.php self-update
 RESULT=$?
-rm composer-setup.php
-exit $RESULT
 
+mv -f composer.phar /usr/share/composer.phar
+ln -s /usr/share/composer.phar ./composer.phar
+rm composer-setup.php
+
+php composer.phar self-update
+
+exit $RESULT
